c6ddb6e3a01652ec4adec79cd3ec1c5266d1f511
nicole lin <nsl.nicoleslin@gmail.com>
2022-05-15 10:56:35 -0700

    code for display

--- dot_display_fw_arduino/single_pixel/single_pixel.ino
+++ dot_display_fw_arduino/single_pixel/single_pixel.ino
@@ -18,18 +18,7 @@
 #define NUM_COL 5
 #define NUM_ROW 7
 #define NUM_CHAR 16
-#define MAX_PERIOD_US 1000 // actual max is 2000
 
-uint8_t n_pot_val = 0;
-unsigned long pot_start_time = 0;
-
-unsigned long col_start_time = 0;
-short col_pin_array[5] = {MCU_COL_1_PIN, MCU_COL_2_PIN, MCU_COL_3_PIN, MCU_COL_4_PIN, MCU_COL_5_PIN};
-uint16_t col_on_us = 10000; // us
-uint16_t num_row_bits = NUM_ROW * NUM_CHAR;
-
-short EEPROM_WRITE_FLAG = 1;
-unsigned long shift_us = 0;
 short data_bit = 0;
 
 void setup() {
@@ -60,6 +49,14 @@ void setup() {
   digitalWrite(MCU_COL_3_PIN, HIGH);
   digitalWrite(MCU_COL_4_PIN, HIGH);
   digitalWrite(MCU_COL_5_PIN, HIGH);
+
+  delay(1);
+  digitalWrite(MCU_DATA_PIN, HIGH);
+  delay(1);
+  digitalWrite(CLK_PIN, HIGH);
+  delay(2);
+  digitalWrite(CLK_PIN, LOW);
+  digitalWrite(MCU_COL_1_PIN, LOW);
 }
 
 void loop() {
@@ -68,32 +65,5 @@ void loop() {
   // load all row data for col 1 (7 bits per character, 16 characters, 112 bits)
   // illuminate col 1
   // repeat for col 2-5
-
-  for(int i = 0; i < NUM_COL; i++)
-  {
-    data_bit = 0;
-    col_start_time = micros();
-    for(int j = 0; j < num_row_bits; j++) 
-    {
-      digitalWrite(MCU_DATA_PIN, data_bit);
-      delayMicroseconds(100);
-      digitalWrite(CLK_PIN, HIGH);
-      data_bit = !data_bit; // toggle the data bit to achieve checkerboard
-      delayMicroseconds(200);
-      digitalWrite(CLK_PIN, LOW);
-    }
-    shift_us = micros() - col_start_time;
-    // digitalWrite(nBLNK_PIN, HIGH);
-    digitalWrite(col_pin_array[i], LOW);
-    if (EEPROM_WRITE_FLAG == 1){
-      EEPROM.write(EEPROM_START_ADDR, shift_us);
-      EEPROM_WRITE_FLAG = 0;
-    }
-    delayMicroseconds(col_on_us);
-    digitalWrite(col_pin_array[i], HIGH);
-    // digitalWrite(nBLNK_PIN, LOW);
-    // delayMicroseconds(MAX_PERIOD_US - col_on_us);
-  }
-  
   
 }
